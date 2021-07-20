d38721f4684c6cdb8097d9b6a4e4c822a9f22968
Георги Тучев <78377593+Tuchev@users.noreply.github.com>
2021-07-20 16:31:24 +0300

    Delete 03. Inheritance directory

--- 03. Inheritance/02. Exercise/project_04.Need_for_Speed/race_motorcycle.py
+++
@@ -1,9 +0,0 @@
-from project.motorcycle import Motorcycle
-
-
-class RaceMotorcycle(Motorcycle):
-    DEFAULT_FUEL_CONSUMPTION = 8
-
-    def __init__(self, fuel, horse_power):
-        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
-        super().__init__(fuel, horse_power)
